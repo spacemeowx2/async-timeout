7ff664c6cd33cea70e37ad30f88580d8b2cba755
spacemeowx2 <spacemeowx2@gmail.com>
2020-07-21 16:33:18 +0800

    add VisitorTimeout

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,57 @@
 use tokio::time::{Instant, Duration, Delay, delay_for};
-use std::{io, pin::Pin, task::{Poll, Context}, future::Future};
+use std::{io, pin::Pin, task::{Poll, Context}, future::Future, sync::{Arc, Mutex}};
+
+#[derive(Debug)]
+pub struct VisitorTimeout {
+    last_visit: Arc<Mutex<Instant>>,
+    timeout: Duration,
+    timer: Delay,
+}
+
+#[derive(Debug, Clone)]
+pub struct Visitor {
+    last_visit: Arc<Mutex<Instant>>,
+}
+
+impl VisitorTimeout {
+    pub fn new(timeout: Duration) -> (VisitorTimeout, Visitor)
+    {
+        let last_visit = Arc::new(Mutex::new(Instant::now()));
+        (VisitorTimeout {
+            last_visit: last_visit.clone(),
+            timeout,
+            timer: delay_for(timeout),
+        }, Visitor {
+            last_visit
+        })
+    }
+    pub fn poll_timeout(&mut self,  cx: &mut Context<'_>) -> Poll<io::Result<()>> {
+        loop {
+            if let Poll::Pending = Pin::new(&mut self.timer).poll(cx) {
+                return Poll::Pending;
+            }
+            let elapsed = self.last_visit.lock().unwrap().elapsed();
+            if elapsed > self.timeout {
+                return Poll::Ready(Err(io::ErrorKind::TimedOut.into()))
+            } else {
+                self.timer = delay_for(self.timeout - elapsed);
+            }
+        }
+    }
+}
+
+impl Future for VisitorTimeout {
+    type Output = io::Result<()>;
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        self.poll_timeout(cx)
+    }
+}
+
+impl Visitor {
+    pub fn visit(&self) {
+        *self.last_visit.lock().unwrap() = Instant::now();
+    }
+}
 
 #[derive(Debug)]
 pub struct Timeout {
